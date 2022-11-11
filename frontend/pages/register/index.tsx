44717cc5eac751767a9d462b5fbb9f04214c36a4
Aman-Mandal <002amankumar@gmail.com>
2022-11-11 23:26:38 +0530

    Added the styling on the remaining form

--- frontend/pages/register/index.tsx
+++ frontend/pages/register/index.tsx
@@ -2,6 +2,38 @@ import Image from "next/image";
 import React from "react";
 import me from "../../public/pic.jpg";
 
+const INPUT: {
+  id: string;
+  type: string;
+  label: string;
+}[] = [
+  {
+    id: "name",
+    type: "text",
+    label: "Full Name",
+  },
+  {
+    id: "email",
+    type: "email",
+    label: "Email",
+  },
+  {
+    id: "btt",
+    type: "checkbox",
+    label: "BitTorrent Token",
+  },
+  {
+    id: "price",
+    type: "number",
+    label: "Price",
+  },
+  {
+    id: "amount",
+    type: "number",
+    label: "Amount",
+  },
+];
+
 const RegistrationFrom: React.FC = () => {
   const registerHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -9,9 +41,13 @@ const RegistrationFrom: React.FC = () => {
     console.log("Sumbitted");
   };
 
+  const labelStyle: string = "font-semibold text-sm mb-1 text-gray-600";
+  const inputStyle: string =
+    "border border-gray-400 p-2 w-full rounded-lg mb-3";
+
   return (
-    <div className="w-[80%] mx-auto flex rounded-lg bg-white h-[88vh] gap-10 p-3">
-      <div className="flex-[0.3] bg-[#3c37ff] rounded-lg px-9 py-6 flex flex-col justify-between">
+    <div className="w-[80%] mx-auto flex rounded-xl bg-white h-full gap-10 p-3">
+      <div className="flex-[0.33] bg-[#3c37ff] rounded-xl px-9 py-6 flex flex-col justify-between">
         <p className="text-white text-2xl font-semibold lowercase">Logo</p>
         <div>
           <h2 className="text-white text-4xl font-semibold mb-4">
@@ -43,33 +79,89 @@ const RegistrationFrom: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="flex-[0.7]">
-        <h2>Register Here</h2>
+      <div className="flex-[0.67] pr-10 py-4">
+        <h2 className="text-3xl font-semibold mb-6">Register Here</h2>
 
         <form onSubmit={registerHandler}>
-          <label htmlFor="name">Full Name</label>
-          <input type="text" id="name" />
-
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-
-          <label htmlFor="payment">Payment Methods</label>
-          <div id="payment">
-            <div>Appname</div>
-            <div>UPI</div>
-            <div>Paypal</div>
+          <div className="flex flex-col">
+            <label className={labelStyle} htmlFor="name">
+              Full Name
+            </label>
+            <input
+              className={inputStyle}
+              type="text"
+              id="name"
+              placeholder="John Doe"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className={labelStyle}>
+              Email
+            </label>
+            <input
+              placeholder="john@email.com"
+              className={inputStyle}
+              type="email"
+              id="email"
+            />
           </div>
 
-          <label htmlFor="token-address">Token Address</label>
-          <input type="text" id="token-address" />
+          <div>
+            <label htmlFor="payment" className={labelStyle}>
+              Payment Methods
+            </label>
+            <div className="relative  flex gap-4" id="payment">
+              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
+                <input type="radio" className="mr-1" />
+                AppName
+              </label>
+              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
+                <input type="radio" className="mr-1" />
+                UPI
+              </label>
+              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
+                <input type="radio" className="mr-1" />
+                Paypal
+              </label>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <input className="" type="checkbox" id="btt" value="btt-token" />
+            <label className={labelStyle}>BitTorrent Token</label>
+          </div>
 
-          <label>Prize</label>
-          <input type="text" />
-
-          <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" />
-
-          <button type="submit">Register</button>
+          <label htmlFor="token-address" className={labelStyle}>
+            Token Address
+          </label>
+          <input className={inputStyle} type="text" id="token-address" />
+          <div className="flex gap-10">
+            <div className="flex flex-col ">
+              <label htmlFor="price" className={labelStyle}>
+                Price
+              </label>
+              <input
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                id="price"
+                type="number"
+              />
+            </div>
+            <div>
+              <label htmlFor="amount" className={labelStyle}>
+                Amount
+              </label>
+              <input
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                type="number"
+                id="amount"
+              />
+            </div>
+          </div>
+          <button
+            className="py-2 mt-4 w-[15rem] text-lg text-white bg-[#3c37ff] rounded-md hover:bg-[#2a269e]"
+            type="submit"
+          >
+            Register
+          </button>
         </form>
       </div>
     </div>
