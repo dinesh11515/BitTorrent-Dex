f7114397bed84100b1c9515c59b54e699b284291
dinesh11515 <dineshaitham2@gmail.com>
2022-11-13 22:27:26 +0530

    added

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -2,38 +2,38 @@ import Link from "next/link";
 import { useContext } from "react";
 import { dexContext } from "../Layout/Layout";
 const Navbar: React.FC = () => {
-    const { connect }: any = useContext(dexContext);
-    return (
-      <nav className=" text-white bg-[#1e1e1e] py-4 ">
-        <div className="flex justify-between w-[90%] mx-auto items-center">
-          <h2 className="font-Grotesk font-semibold text-2xl">BitTorrent Dex</h2>
-  
-          <div className="flex gap-10 items-center">
-            <ul className="flex gap-14 uppercase font-thin tracking-widest">
-              <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
-                <Link href="/">Home</Link>
-              </li>
-              <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
-                <Link href="/">Buy</Link>
-              </li>
-              <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
-                <Link href="/register">Sell</Link>
-              </li>
-              <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
-                <Link href="/">About</Link>
-              </li>
-            </ul>
-          </div>
-  
-          <button
-            onClick={connect}
-            className="bg-gray-600 text-white text-lg px-4 py-2 rounded-md font-Grotesk font-medium hover:scale-105 hover:bg-gray-300 hover:text-black"
-          >
-            Connect Wallet
-          </button>
+  const { connect }: any = useContext(dexContext);
+  return (
+    <nav className=" text-white bg-[#1e1e1e] py-4 ">
+      <div className="flex justify-between w-[90%] mx-auto items-center">
+        <h2 className="font-Grotesk font-semibold text-2xl">BitTorrent Dex</h2>
+
+        <div className="flex gap-10 items-center">
+          <ul className="flex gap-14 uppercase font-thin tracking-widest">
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/">Buy</Link>
+            </li>
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/register">Sell</Link>
+            </li>
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/">About</Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+
+        <button
+          onClick={connect}
+          className="bg-gray-600 text-white text-lg px-4 py-2 rounded-md font-Grotesk font-medium hover:scale-105 hover:bg-gray-300 hover:text-black"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
