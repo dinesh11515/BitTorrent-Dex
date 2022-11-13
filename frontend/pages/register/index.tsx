014c650fc2df9334bbdd0920f11bd76c33b1c011
Aman-Mandal <002amankumar@gmail.com>
2022-11-13 14:10:28 +0530

    Form Completed

--- frontend/pages/register/index.tsx
+++ frontend/pages/register/index.tsx
@@ -1,44 +1,93 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import me from "../../public/pic.jpg";
-
-const INPUT: {
-  id: string;
-  type: string;
-  label: string;
-}[] = [
-  {
-    id: "name",
-    type: "text",
-    label: "Full Name",
-  },
-  {
-    id: "email",
-    type: "email",
-    label: "Email",
-  },
-  {
-    id: "btt",
-    type: "checkbox",
-    label: "BitTorrent Token",
-  },
-  {
-    id: "price",
-    type: "number",
-    label: "Price",
-  },
-  {
-    id: "amount",
-    type: "number",
-    label: "Amount",
-  },
-];
+import paytm from "../../public/paytm.png";
+import gpay from "../../public/gpay.png";
+import phonepe from "../../public/phonepe.png";
+import paypal from "../../public/paypal.png";
 
 const RegistrationFrom: React.FC = () => {
+  const [showPaytmUPI, setShowPaytmUPI] = useState<boolean>(false);
+  const [showGpayUPI, setShowGpayUPI] = useState<boolean>(false);
+  const [showPaypalUPI, setShowPaypalUPI] = useState<boolean>(false);
+  const [showPhonepeUPI, setShowPhonepeUPI] = useState<boolean>(false);
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const tokenAddressInputRef = useRef<HTMLInputElement>(null);
+  const priceInputRef = useRef<HTMLInputElement>(null);
+  const amountInputRef = useRef<HTMLInputElement>(null);
+  const gpayInputRef = useRef<HTMLInputElement>(null);
+  const phonepeInputRef = useRef<HTMLInputElement>(null);
+  const paytmInputRef = useRef<HTMLInputElement>(null);
+  const paypalInputRef = useRef<HTMLInputElement>(null);
+
+  const paytmUPIHandler: () => void = () => {
+    setShowPaytmUPI(!showPaytmUPI);
+    setShowGpayUPI(false);
+    setShowPaypalUPI(false);
+    setShowPhonepeUPI(false);
+  };
+
+  const gpayUPIHandler: () => void = () => {
+    setShowGpayUPI(!showGpayUPI);
+    setShowPaytmUPI(false);
+    setShowPaypalUPI(false);
+    setShowPhonepeUPI(false);
+  };
+
+  const phonepeUPIHandler: () => void = () => {
+    setShowPhonepeUPI(!showPhonepeUPI);
+    setShowGpayUPI(false);
+    setShowPaytmUPI(false);
+    setShowPaypalUPI(false);
+  };
+
+  const paypalUPIHandler: () => void = () => {
+    setShowPaypalUPI(!showPaypalUPI);
+    setShowGpayUPI(false);
+    setShowPaytmUPI(false);
+    setShowPhonepeUPI(false);
+  };
+
+  const checkboxHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
   const registerHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    console.log("Sumbitted");
+    let enteredName = nameInputRef.current!.value;
+    let enteredEmail = emailInputRef.current!.value;
+    let enteredPrice = priceInputRef.current!.value;
+    let enteredAmount = amountInputRef.current!.value;
+    let enteredGpayUPI = gpayInputRef.current?.value;
+    let enteredPhonepeUPI = phonepeInputRef.current?.value;
+    let enteredPaypalUPI = paypalInputRef.current?.value;
+    let enteredPaytmUPI = paytmInputRef.current?.value;
+    let enteredToken = tokenAddressInputRef.current?.value;
+
+    if (
+      enteredName.trim().length === 0 ||
+      enteredEmail.trim().length === 0 ||
+      enteredPrice.length === 0 ||
+      enteredAmount.length === 0
+    )
+      return;
+
+    if (
+      enteredGpayUPI?.trim().length === 0 ||
+      enteredPhonepeUPI?.trim().length === 0 ||
+      enteredPaypalUPI?.trim().length === 0 ||
+      enteredPaytmUPI?.trim().length === 0
+    )
+      return alert("Please enter UPI address");
+
+    if (!isChecked && enteredToken?.length === 0)
+      return alert("Enter Token Address or use BTT Token");
+
+    console.log("Submitted");
   };
 
   const labelStyle: string = "font-semibold text-sm mb-1 text-gray-600";
@@ -46,7 +95,7 @@ const RegistrationFrom: React.FC = () => {
     "border border-gray-400 p-2 w-full rounded-lg mb-3";
 
   return (
-    <div className="w-[80%] mx-auto flex rounded-xl bg-white h-full gap-10 p-3">
+    <div className="w-[80%] mx-auto flex rounded-xl bg-white h-screen gap-10 p-3">
       <div className="flex-[0.33] bg-[#3c37ff] rounded-xl px-9 py-6 flex flex-col justify-between">
         <p className="text-white text-2xl font-semibold lowercase">Logo</p>
         <div>
@@ -82,12 +131,15 @@ const RegistrationFrom: React.FC = () => {
       <div className="flex-[0.67] pr-10 py-4">
         <h2 className="text-3xl font-semibold mb-6">Register Here</h2>
 
+        {/* Form */}
         <form onSubmit={registerHandler}>
           <div className="flex flex-col">
             <label className={labelStyle} htmlFor="name">
               Full Name
             </label>
             <input
+              required
+              ref={nameInputRef}
               className={inputStyle}
               type="text"
               id="name"
@@ -99,6 +151,8 @@ const RegistrationFrom: React.FC = () => {
               Email
             </label>
             <input
+              required
+              ref={emailInputRef}
               placeholder="john@email.com"
               className={inputStyle}
               type="email"
@@ -110,36 +164,103 @@ const RegistrationFrom: React.FC = () => {
             <label htmlFor="payment" className={labelStyle}>
               Payment Methods
             </label>
-            <div className="relative  flex gap-4" id="payment">
-              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
-                <input type="radio" className="mr-1" />
-                AppName
-              </label>
-              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
-                <input type="radio" className="mr-1" />
-                UPI
-              </label>
-              <label className="px-3 py-2 my-4 w-[10rem] flex items-center gap-1 rounded-lg border cursor-pointer border-gray-400">
-                <input type="radio" className="mr-1" />
-                Paypal
-              </label>
+
+            <div className="flex w-full gap-4 items-center mb-4">
+              <div
+                onClick={paytmUPIHandler}
+                className="border border-gray-400 rounded-lg py-4 px-6 cursor-pointer hover:bg-gray-200"
+              >
+                <Image src={paytm} width={80} alt="paytm" />
+              </div>
+
+              <div
+                onClick={phonepeUPIHandler}
+                className="border border-gray-400 rounded-lg py-2 px-6 cursor-pointer hover:bg-gray-200"
+              >
+                <Image src={phonepe} width={50} height={50} alt="phonepe" />
+              </div>
+
+              <div
+                onClick={gpayUPIHandler}
+                className="border border-gray-400 rounded-lg py-2 px-6 cursor-pointer hover:bg-gray-200"
+              >
+                <Image src={gpay} width={50} height={50} alt="gpay" />
+              </div>
+
+              <div
+                onClick={paypalUPIHandler}
+                className="border border-gray-400 rounded-lg py-2 px-6 cursor-pointer hover:bg-gray-200"
+              >
+                <Image src={paypal} width={50} height={50} alt="paypal" />
+              </div>
             </div>
+            {showPaytmUPI && (
+              <input
+                ref={paytmInputRef}
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                type="text"
+                placeholder="abcd@paytm"
+              />
+            )}
+
+            {showGpayUPI && (
+              <input
+                ref={gpayInputRef}
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                type="text"
+                placeholder="abcd@oksbi.com"
+              />
+            )}
+
+            {showPaypalUPI && (
+              <input
+                ref={paypalInputRef}
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                type="text"
+                placeholder="abcd@paypal"
+              />
+            )}
+
+            {showPhonepeUPI && (
+              <input
+                ref={phonepeInputRef}
+                className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
+                type="text"
+                placeholder="abcd@phonepe"
+              />
+            )}
           </div>
-          <div className="flex items-center gap-1">
-            <input className="" type="checkbox" id="btt" value="btt-token" />
-            <label className={labelStyle}>BitTorrent Token</label>
+          <div className="py-3">
+            <label className={`${labelStyle}`}>
+              <input
+                onChange={checkboxHandler}
+                className="inline-block h-4 w-4 mr-2 rounded-lg"
+                type="checkbox"
+                id="btt"
+                value="btt"
+                checked={isChecked}
+              />
+              BitTorrent Token
+            </label>
           </div>
 
           <label htmlFor="token-address" className={labelStyle}>
             Token Address
           </label>
-          <input className={inputStyle} type="text" id="token-address" />
+          <input
+            ref={tokenAddressInputRef}
+            className={inputStyle}
+            type="text"
+            id="token-address"
+          />
           <div className="flex gap-10">
             <div className="flex flex-col ">
               <label htmlFor="price" className={labelStyle}>
                 Price
               </label>
               <input
+                required
+                ref={priceInputRef}
                 className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
                 id="price"
                 type="number"
@@ -150,6 +271,8 @@ const RegistrationFrom: React.FC = () => {
                 Amount
               </label>
               <input
+                required
+                ref={amountInputRef}
                 className={`border border-gray-400 p-2 w-[18rem] rounded-lg`}
                 type="number"
                 id="amount"
